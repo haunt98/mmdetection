c211a3ea879b4c95feff60387b87e2669f4087f7
Hau Nguyen <hauvipapro@gmail.com>
2024-05-25 19:47:55 +0700

    feat: add manga109 config

---
+++ configs/manga109/yolox_tiny_8xb8-300e_manga109.py
@@ -0,0 +1,41 @@
+_base_ = ["../yolox/yolox_tiny_8xb8-300e_coco.py"]
+
+
+model = dict(roi_head=dict(bbox_head=dict(num_classes=4)))
+
+data_root = "data/Manga109s/"
+metainfo = {
+    "classes": ("body", "face", "frame", "text"),
+}
+
+train_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file="annotations_coco_format/manga109s_coco_90train.json",
+        data_prefix=dict(img="images/"),
+    ),
+)
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file="annotations_coco_format/manga109s_coco_4val.json",
+        data_prefix=dict(img="images/"),
+    )
+)
+test_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file="annotations_coco_format/manga109s_coco_15test.json",
+        data_prefix=dict(img="images/"),
+    )
+)
+
+val_evaluator = dict(ann_file=data_root + "val/manga109s_coco_4val.json")
+test_evaluator = dict(ann_file=data_root + "val/manga109s_coco_15test.json")
+
+# https://github.com/open-mmlab/mmdetection/tree/main/configs/yolox
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_tiny_8x8_300e_coco/yolox_tiny_8x8_300e_coco_20211124_171234-b4047906.pth"
